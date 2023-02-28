f38802d0820a53491221ed7fe8d69dcb5aa64fbd
Beth Barker <barker.k.elizabeth@gmail.com>
2023-02-28 15:41:31 -0700

    added demo files

---
+++ intro.js
@@ -0,0 +1,30 @@
+
+//console.log("Hello, World!")
+
+const firstName = "Beth"
+
+//console.log(firstName)
+
+let age = 27
+
+const isCool = true
+
+//console.log(firstName, age, isCool)
+
+// console.log("Hello, my name is " + firstName + " I am " + age + " years old.")
+// console.log(`Hello, my name is ${firstName} I am ${age} years old.`)
+
+let x = 4
+let y = 10
+let z = x + 8
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// console.log(sum)
+
+if(x === y){
+    console.log('X and Y are the same value')
+}
